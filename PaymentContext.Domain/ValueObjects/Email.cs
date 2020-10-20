d1466196d0f1555a7d7207b3e8bbc1e7f78cb701
Sandro-Sousa <sandro-souza-oliveira@hotmail.com>
2020-10-20 14:44:58 -0300

    Modeling Domains

---
+++ PaymentContext.Domain/ValueObjects/Email.cs
@@ -0,0 +1,21 @@
+using System.Diagnostics.Contracts;
+using Flunt.Validations;
+using PaymentContext.Shared.ValueObjects;
+
+namespace PaymentContext.Domain.ValueObjects
+{
+    public class Email : ValueObject
+    {
+        public Email(string address)
+        {
+            Address = address;
+
+            AddNotifications(new Flunt.Validations.Contract()
+                .Requires()
+                .IsEmail(Address, "Email.Address", "E-mail inválido")
+            );
+        }
+
+        public string Address { get; private set; }
+    }
+}
---
+++ PaymentContext.Tests/ValueObjects/DocumentTests.cs
@@ -0,0 +1,47 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PaymentContext.Domain.Entities;
+using PaymentContext.Domain.Enums;
+using PaymentContext.Domain.ValueObjects;
+
+namespace PaymentContext.Tests
+{
+    [TestClass]
+    public class DocumentTests
+    {
+        // Red, Green, Refactor
+
+        [TestMethod]
+        public void ShouldReturnErrorWhenCNPJIsInvalid()
+        {
+            var doc = new Document("123", EDocumentType.CNPJ);
+            Assert.IsTrue(doc.Invalid);
+        }
+
+
+        [TestMethod]
+        public void ShouldReturnSuccessWhenCNPJIsValid()
+        {
+            var doc = new Document("99640653000100", EDocumentType.CNPJ);
+            Assert.IsTrue(doc.Valid);
+        }
+
+        [TestMethod]
+        public void ShouldReturnErrorWhenCPFIsInvalid()
+        {
+            var doc = new Document("123", EDocumentType.CPF);
+            Assert.IsTrue(doc.Invalid);
+        }
+
+
+        [TestMethod]
+        [DataTestMethod]
+        [DataRow("97868957034")]
+        [DataRow("98043572089")]
+        [DataRow("48122867006")]
+        public void ShouldReturnSuccessWhenCPFIsValid(string cpf)
+        {
+            var doc = new Document(cpf, EDocumentType.CPF);
+            Assert.IsTrue(doc.Valid);
+        }
+    }
+}
